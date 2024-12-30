a9d5426134cbc2283e5a287e499002022896c830
NPerfei <regulacionperfei@gmail.com>
2024-12-30 18:03:30 +0800

    Started finalizing index.html

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav a");
+const projectLists = document.querySelectorAll(".project-list")
 
 const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 navLinks.forEach((link) => link.classList.remove("active"));
-                sections.forEach((section) => {section.firstElementChild.classList.remove("active")})
+                projectLists.forEach((projectList) => projectList.classList.remove("show"));
 
                 const id = entry.target.id;
                 document.querySelector(`nav a[href="#${id}"]`).classList.add('active');
-                document.getElementById(`${id}`).firstElementChild.classList.add('active');
+
+                if (id == "web-projects") {
+                    projectLists[0].classList.add("show");
+                }
+                else if (id == "github-projects") {
+                    projectLists[1].classList.add("show");
+                }
             }
         });
     },
     {
-        threshold: 0.7,
+        threshold: 0.6,
     }
 );
 
